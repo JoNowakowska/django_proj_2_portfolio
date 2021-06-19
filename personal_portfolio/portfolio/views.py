3a9f34e34ede8a3dc760869f1ac26c1d9808c6e9
Joanna <joanna.k.nowakowska@gmail.com>
2021-06-19 19:35:51 +0000

    .gitignore modified again

--- personal_portfolio/portfolio/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-import datetime
-
-
-def home(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {"date": datetime.date.today(), "projects": projects})
